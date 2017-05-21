f64e6b06524c2eda7757b6ed8b0f93d17e399375
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-21 11:21:36 -0300

    Correção de renderização HTML.

--- routes.js
+++ routes.js
@@ -2,24 +2,30 @@ const Router = require('express').Router();
 const request = require('request');
 const pug = require('pug');
 
+// Rota principal de App1.
 Router.get('/app1', function(req, res) {
+  // Realiza requisição em API externa.
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
+  // Converte resultado de requisição em JSON.
   body = JSON.parse(body);
-  res.render('index_users', { title: 'Usu&aacute;rios', message: 'Usu&aacute;rios', item: body})
+  // Renderiza página HTML.
+  res.render('index_users', { item: body})
     });
 });
 
+// Rota 'users' de App1.
 Router.get('/app1/users', function(req, res) {
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
   body = JSON.parse(body);
-  res.render('index_users', { title: 'Usu&aacute;rios', message: 'Usu&aacute;rios', item: body})
+  res.render('index_users', { item: body})
     });
 });
 
-Router.get('/app1/vehicles', function(req, res) {
+// Rota 'vehicles' de App1.
+Router.get('/app1/vehicles', function(req, res) { 
   request('https://private-bc4b79-automobidummy.apiary-mock.com/vehicles', function (error, response, body) {
   body = JSON.parse(body);
-  res.render('index_vehicle', { title: 'Ve&iacute;culos', message: 'Ve&iacute;culos', item: body})
+  res.render('index_vehicle', { item: body})
     });
 });
 
