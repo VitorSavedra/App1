e251e2392e34b5ccd973a045d4feecde1d05d9ee
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-21 00:41:03 -0300

    Melhorias, correções, rotas e +

--- routes.js
+++ routes.js
@@ -2,14 +2,21 @@ const Router = require('express').Router();
 const request = require('request');
 const pug = require('pug');
 
-Router.get('/users', function(req, res) {
+Router.get('/app1', function(req, res) {
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
   body = JSON.parse(body);
   res.render('index_users', { title: 'Usuários', message: 'Usuários', item: body})
     });
 });
 
-Router.get('/vehicles', function(req, res) {
+Router.get('/app1/users', function(req, res) {
+  request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
+  body = JSON.parse(body);
+  res.render('index_users', { title: 'Usuários', message: 'Usuários', item: body})
+    });
+});
+
+Router.get('/app1/vehicles', function(req, res) {
   request('https://private-bc4b79-automobidummy.apiary-mock.com/vehicles', function (error, response, body) {
   body = JSON.parse(body);
   res.render('index_vehicle', { title: 'Veículos', message: 'Veículos', item: body})
