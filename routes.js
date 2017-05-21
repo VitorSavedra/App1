32e952a19263e1b2a8dae05e7e0e7f1af9a5fc50
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-21 01:05:05 -0300

    Correção de títulos.

--- routes.js
+++ routes.js
@@ -5,21 +5,21 @@ const pug = require('pug');
 Router.get('/app1', function(req, res) {
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
   body = JSON.parse(body);
-  res.render('index_users', { title: 'Usuários', message: 'Usuários', item: body})
+  res.render('index_users', { title: 'Usuarios', message: 'Usuarios', item: body})
     });
 });
 
 Router.get('/app1/users', function(req, res) {
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
   body = JSON.parse(body);
-  res.render('index_users', { title: 'Usuários', message: 'Usuários', item: body})
+  res.render('index_users', { title: 'Usuarios', message: 'Usuarios', item: body})
     });
 });
 
 Router.get('/app1/vehicles', function(req, res) {
   request('https://private-bc4b79-automobidummy.apiary-mock.com/vehicles', function (error, response, body) {
   body = JSON.parse(body);
-  res.render('index_vehicle', { title: 'Veículos', message: 'Veículos', item: body})
+  res.render('index_vehicle', { title: 'Veiculos', message: 'Veiculos', item: body})
     });
 });
 
